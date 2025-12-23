cc4ec3fb9ad1364af418379d80e2e1231641e487
Christian Borchgrevink-Vigeland <borchgrevink@gmail.com>
2025-12-23 18:48:19 +0100

    Debug: Retry DB Config with Errors

--- db_config.php
+++ db_config.php
@@ -7,9 +7,14 @@ define('DB_PASS', 'G8cN12%C^a31');
 
 function getDB()
 {
+    // Enable Error Reporting for Debugging
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        die("<h1>DATABASE CONNECTION FAILED</h1><br>Error: " . $conn->connect_error);
     }
     $conn->set_charset("utf8mb4");
     return $conn;
