1e8fae8d94be546ab89730c6f5a31e4bcfe0e3e3
Christian Borchgrevink-Vigeland <borchgrevink@gmail.com>
2025-12-28 12:10:21 +0100

    v3.02: Forcing iOS text casing (CSS override)

--- game_v9.js
+++ game_v9.js
@@ -44,7 +44,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Better: Update the Version text immediately    // Final Version
     const vCheck = document.getElementById('version-number');
-    if (vCheck) vCheck.innerText = "v3.01";
+    if (vCheck) vCheck.innerText = "v3.02";
 
     const canvas = document.getElementById('game-canvas');
     if (!canvas) { log("CRITICAL: Canvas not found!"); return; }
