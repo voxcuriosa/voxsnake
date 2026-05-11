bb6116a87ec175c8af210839dacb556349a36ab0
voxcuriosa <borchgrevink@gmail.com>
2026-05-12 00:26:42 +0200

    Restore missing start buttons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Update cache version to force refresh
-const CACHE_NAME = 'neon-snake-v73-network-first';
+const CACHE_NAME = 'neon-snake-v74-network-first';
 
 const ASSETS_TO_CACHE = [
     './',
